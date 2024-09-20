c8a7e410d26018666177a98a421fde1a8c1272ef
MantraGitH <camilo.arias56@hotmail.com>
2024-09-20 18:32:21 -0300

    Final commit. Unfollow hover button changes

--- x-follow-card/src/App.jsx
+++ x-follow-card/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
 import "./App.css";
 import { XFollowCard } from "./xFollowCard";
 
 export function App() {
-  const [name, setName] = useState("nodejs");
   return (
     <section className="App">
-      <XFollowCard userName="reactjs" initialIsFollowing={true}>React </XFollowCard>
-      <XFollowCard userName={name}>Node</XFollowCard>
-
-      <button onClick={() => setName("github")}>Cambiar Nombre</button>
+      <XFollowCard userName="NASA">NASA</XFollowCard>
+      <XFollowCard userName="Microsoft">Microsoft</XFollowCard>
     </section>
   );
 }
--- x-follow-card/src/XFollowCard.jsx
+++ x-follow-card/src/XFollowCard.jsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
-export function XFollowCard({ children, userName, initialIsFollowing}) {
-  const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
-
+export function XFollowCard({ children, userName }) {
+  const [isFollowing, setIsFollowing] = useState(false); // Inicializar el estado
 
   const text = isFollowing ? "Siguiendo" : "Seguir";
   const buttonClassName = isFollowing
-    ? "x-followCard-button is-Following"
+    ? "x-followCard-button is-following" // Clases en minúsculas
     : "x-followCard-button";
 
-    const handleClick = () => {
-      setIsFollowing(!isFollowing)
-    }
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+
   return (
     <article className="x-followCard">
       <header className="x-followCard-header">
         <img
           className="x-followCard-avatar"
           src={`https://unavatar.io/${userName}`}
-          alt=""
+          alt={`${userName} avatar`}
         />
         <div className="x-followCard-info">
           <strong>{children}</strong>
-          <span className="x-followCard-infoUserName ">@{userName}</span>
+          <span className="x-followCard-infoUserName">@{userName}</span>
         </div>
       </header>
       <aside>
-        <button className={buttonClassName} onClick={handleClick}>{text}</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className="x-followCard-text">{text}</span>
+          <span className="x-followCard-stopFollow">Dejar de seguir</span>
+        </button>
       </aside>
     </article>
   );
