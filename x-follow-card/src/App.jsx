1fee8b54acbf8b84c3e4aec1a72cfdf0ac72c630
MantraGitH <camilo.arias56@hotmail.com>
2024-09-20 16:50:28 -0300

    rerender, changes propagation

--- x-follow-card/src/App.jsx
+++ x-follow-card/src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import { XFollowCard } from "./xFollowCard";
 
 export function App() {
+  const [name, setName] = useState("nodejs");
   return (
     <section className="App">
       <XFollowCard userName="reactjs">React </XFollowCard>
-      <XFollowCard userName="nodejs">Node</XFollowCard>
+      <XFollowCard userName={name}>Node</XFollowCard>
+
+      <button onClick={() => setName("github")}>Cambio Nombre</button>
     </section>
   );
 }
